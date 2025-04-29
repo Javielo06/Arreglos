21ee0c0b631ad43f2e5bd954e9bc7fc251d50e4a
Javielo06 <ing.jmalcazar@gmail.com>
2025-04-28 18:14:27 -0600

    se agrega primer ejemplo de arreglo

--- src/EjemploArreglos.java
+++ src/EjemploArreglos.java
@@ -1,7 +1,23 @@
 public class EjemploArreglos {
     public static void main(String[] args) {
 
-        int [] numeros = new int[4];
+        int [] numeros = new int[4]; //aqui es donde se define el tamaño del arreglo
+        //no se pueden asignar mas valores de los que se establecieron en el tamaño del arreglo
 
+        numeros [0] = 1;
+        numeros [1] = 2;
+        numeros [2] = 3;
+        numeros [3] = 4;
+
+        int i = numeros[0];
+        int j = numeros[1];
+        int k = numeros[2];
+        int l = numeros[3];
+        //no se puede leer mas valores del tamaño definido por el arreglo
+
+        System.out.println("i = " + i);
+        System.out.println("j = " + j);
+        System.out.println("k = " + k);
+        System.out.println("l = " + l);
     }
 }
